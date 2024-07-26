499a5d449df4b42941126e9b6059aaa6459cf3a7
zbw8388 <zbw8388@gmail.com>
2024-07-26 14:52:06 -0700

    chore: bump up version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ print("Additional CMake Arguments = {}".format(cmake_args))
 
 setup(
     name="treefarms",
-    version="0.2.2",
+    version="0.2.3",
     description="Implementation of Trees FAst RashoMon Sets",
     author="UBC Systopia Research Lab",
     url="https://github.com/ubc-systopia/treeFarms",
